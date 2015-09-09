3b9675d6bc0328c515675fbb89313fb4e12b894b
tomkocmi <kocmitom@gmail.com>
2015-09-09 15:45:25 +0200

    edited error in fixes.py

--- src/Fixes.py
+++ src/Fixes.py
@@ -5,10 +5,53 @@ import pickle
 import os
 
 
+# def generateFixes(vocabulary):
+#     if not os.path.isfile(Cons.FIXESFILENAME):
+#         prefixes = []
+#         suffixes = []
+#
+#         for i in range(0, len(vocabulary)):
+#             if i % 100 == 0:
+#                 print "progress: " + str(i) + "/" + str(len(vocabulary))
+#             for j in range(0, len(vocabulary)):
+#                 if i < j:
+#                     if len(vocabulary[i]) < len(vocabulary[j]):
+#                         w1 = vocabulary[i]
+#                         w2 = vocabulary[j]
+#                     else:
+#                         w1 = vocabulary[j]
+#                         w2 = vocabulary[i]
+#                     # extract prefixes
+#                     for fix in range(max(1, len(w1) - Cons.MAXFIX), len(w1) + 1):
+#                         if w1[-fix:] == w2[-fix:]:
+#                             if len(w2[:-fix]) <= Cons.MAXFIX:
+#                                 prefixes.append(["pre", w1[:-fix], w2[:-fix]])
+#                         else:
+#                             break
+#
+#                     # extract suffixes
+#                     for fix in range(max(1, len(w1) - Cons.MAXFIX), len(w1) + 1):
+#                         if w1[:fix] == w2[:fix]:
+#                             if len(w2[fix:]) <= Cons.MAXFIX:
+#                                 suffixes.append(["suf", w1[fix:], w2[fix:]])
+#                         else:
+#                             break
+#
+#         with open(Cons.FIXESFILENAME, 'w') as f:
+#             pickle.dump([prefixes, suffixes], f)
+#     else:
+#         with open(Cons.FIXESFILENAME, 'r') as f:
+#             fixes = pickle.load(f)
+#             prefixes = fixes[0]
+#             suffixes = fixes[1]
+#
+#     return prefixes, suffixes
+
+
 def generateFixes(vocabulary):
     if not os.path.isfile(Cons.FIXESFILENAME):
-        prefixes = []
-        suffixes = []
+        prefixes = {}
+        suffixes = {}
 
         for i in range(0, len(vocabulary)):
             if i % 100 == 0:
@@ -22,20 +65,47 @@ def generateFixes(vocabulary):
                         w1 = vocabulary[j]
                         w2 = vocabulary[i]
                     # extract prefixes
-                    for fix in range(max(1, len(w1) - Cons.MAXFIX), len(w1) + 1):
+                    for fix in range(len(w1) + 1,max(1, len(w1) - Cons.MAXFIX)):
                         if w1[-fix:] == w2[-fix:]:
                             if len(w2[:-fix]) <= Cons.MAXFIX:
-                                prefixes.append(["pre", w1[:-fix], w2[:-fix]])
-                        else:
+                                if (len(w1[:-fix]) == len(w2[:-fix])) and w1[:-fix] > w2[:-fix]:
+                                    pre1 = w2[:-fix]
+                                    pre2 = w1[:-fix]
+                                else:
+                                    pre1 = w1[:-fix]
+                                    pre2 = w2[:-fix]
+
+                                if pre1 not in prefixes:
+                                    prefixes[pre1] = {}
+                                if pre2 not in prefixes[pre1]:
+                                    #prefixes[pre1][pre2] = 1
+                                    prefixes[pre1][pre2] = [[w1, w2]]
+                                else:
+                                    #prefixes[pre1][pre2] += 1
+                                    prefixes[pre1][pre2].append([w1, w2])
+                                    print pre1+" "+pre2
                             break
 
                     # extract suffixes
                     for fix in range(max(1, len(w1) - Cons.MAXFIX), len(w1) + 1):
                         if w1[:fix] == w2[:fix]:
                             if len(w2[fix:]) <= Cons.MAXFIX:
-                                suffixes.append(["suf", w1[fix:], w2[fix:]])
+                                if (len(w1[fix:]) == len(w2[fix:])) and w1[fix:] > w2[fix:]:
+                                    pre1 = w2[fix:]
+                                    pre2 = w1[fix:]
+                                else:
+                                    pre1 = w1[fix:]
+                                    pre2 = w2[fix:]
+
+                                if pre1 not in suffixes:
+                                    suffixes[pre1] = {}
+                                if pre2 not in suffixes[pre1]:
+                                    suffixes[pre1][pre2] = [[w1, w2]]
+                                else:
+                                    suffixes[pre1][pre2].append([w1,w2])
                         else:
                             break
+
 
         with open(Cons.FIXESFILENAME, 'w') as f:
             pickle.dump([prefixes, suffixes], f)
@@ -44,39 +114,5 @@ def generateFixes(vocabulary):
             fixes = pickle.load(f)
             prefixes = fixes[0]
             suffixes = fixes[1]
-
-    return prefixes, suffixes
-
-
-def generateFixes2(vocabulary):
-    prefixes = {}
-    suffixes = {}
-
-    for i in range(0, len(vocabulary)):
-        if i % 100 == 0:
-            print "progress: " + str(i) + "/" + str(len(vocabulary))
-        for j in range(0, len(vocabulary)):
-            if i < j:
-                if len(vocabulary[i]) < len(vocabulary[j]):
-                    w1 = vocabulary[i]
-                    w2 = vocabulary[j]
-                else:
-                    w1 = vocabulary[j]
-                    w2 = vocabulary[i]
-                # extract prefixes
-                for fix in range(max(1, len(w1) - Cons.MAXFIX), len(w1) + 1):
-                    if w1[-fix:] == w2[-fix:]:
-                        if len(w2[:-fix]) <= Cons.MAXFIX:
-                            prefixes[w1[:-fix], w2[:-fix]]+=1;
-                    else:
-                        break
-
-                # extract suffixes
-                for fix in range(max(1, len(w1) - Cons.MAXFIX), len(w1) + 1):
-                    if w1[:fix] == w2[:fix]:
-                        if len(w2[fix:]) <= Cons.MAXFIX:
-                            suffixes[w1[fix:], w2[fix:]]
-                    else:
-                        break
 
     return prefixes, suffixes
