2e22dc2efd94ce73b97943c512a1520c647ea40e
tomkocmi <kocmitom@gmail.com>
2015-09-09 16:35:31 +0200

    test

--- src/Cons.py
+++ src/Cons.py
@@ -1,9 +1,9 @@
 __author__="Tom Kocmi"
 
 
-MODELFILENAME = "tmp/VectorModel.data" #model filename
-FIXESFILENAME = "tmp/Fixes.data" #filename of saved prefixes and suffixes
-DATAFILENAME = "tmp/orwell.txt" #filename of training data
+MODELFILENAME = "models/VectorModel.data" #model filename
+FIXESFILENAME = "models/Fixes.data" #filename of saved prefixes and suffixes
+DATAFILENAME = "models/orwell.txt" #filename of training data
 
 NNSIZE = 100 #size of the word vectors
 MINOCCURENCES = 1 #minimum number of occurences of word in the document
